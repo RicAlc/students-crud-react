f289070c38c0ce59231d0449c01b3004f750e76b
RicAlc <alcala.ricardoc@gmail.com>
2022-08-06 19:56:17 -0500

    3. Probando todos los metodos CRUD de la api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import getAllStudents from './functions/services';
+import {
+  createStudent,
+  deleteStudent,
+  getAllStudents,
+  getStudent,
+  updateStudent,
+} from './functions/services';
 
 function App() {
   const [dataDB, setDataDB] = useState(null);
+  const [student, setStudent] = useState(null);
+  useEffect(() => {
+    getStudent(1, setStudent);
+  }, []);
   useEffect(() => {
     getAllStudents(setDataDB);
   }, []);
+  const addStudent = () => {
+    createStudent('Anotnio', 'Cruz', true);
+  };
+  const removeStudent = () => {
+    deleteStudent(5);
+  };
+  const editStudent = () => {
+    updateStudent(3, 'Ivan', 'Solar', false);
+    return true;
+  };
+  function findStudent() {
+    console.log(student.data[0].nameST);
+  }
 
   function setPageContent(data) {
     if (data) {
@@ -16,6 +39,16 @@ function App() {
         <>
           <h1>Estudiantes</h1>
           {studentsMapped}
+          <button onClick={addStudent} className='add'>
+            Add student
+          </button>
+          <button onClick={removeStudent} className='delete'>
+            Delete student
+          </button>
+          <button onClick={editStudent} className='update'>
+            Edit student
+          </button>
+          <button onClick={findStudent}>Single student</button>
         </>
       );
     } else {
--- src/functions/services.js
+++ src/functions/services.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const URI = 'http://localhost:4000/students';
+const URI = 'http://localhost:4000/students/';
+
 const getAllStudents = async (state) => {
   try {
     const res = await axios.get(URI);
@@ -9,5 +10,44 @@ const getAllStudents = async (state) => {
     console.error(error);
   }
 };
+const getStudent = async (id, state) => {
+  try {
+    const res = await axios.get(URI + id);
+    state(res);
+  } catch (error) {
+    console.error(error);
+  }
+};
+const createStudent = async (name, lastName, active) => {
+  try {
+    await axios.post(URI, {
+      nameST: name,
+      lastNameST: lastName,
+      activeST: active,
+    });
+  } catch (error) {}
+};
+const updateStudent = async (id, name, lastName, active) => {
+  try {
+    await axios.put(URI + id, {
+      nameST: name,
+      lastNameST: lastName,
+      activeST: active,
+    });
+  } catch (error) {}
+};
+const deleteStudent = async (id) => {
+  try {
+    await axios.delete(URI + id);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export default getAllStudents;
+export {
+  getAllStudents,
+  getStudent,
+  updateStudent,
+  createStudent,
+  deleteStudent,
+};
